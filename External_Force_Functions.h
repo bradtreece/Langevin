16db93ebadb1497bb311bf57a91950eaf18e349f
BRAD <brad.treece@yahoo.com>
2019-06-04 18:16:40 -0400

    Adding Linear Forces

--- External_Force_Functions.h
+++ External_Force_Functions.h
@@ -25,4 +25,25 @@ class Constant_Force : public Force_External {
     };
 };
 
+class Linear_Force : public Force_External {
+  private:
+    double constant_coeff;
+    double linear_coeff;
+  public:
+    Linear_Force(double linear_coefficient, double constant_coefficient)
+    : constant_coeff{constant_coefficient}
+    , linear_coeff{linear_coeff}
+    {}
+
+    Linear_Force(double linear_coefficient)
+    : Linear_Force(linear_coefficient, 0.0) {}
+
+    Linear_Force()
+    : Linear_Force(0.0, 0.0) {}
+
+    double force_calculation (double x) override {
+        return linear_coeff*x + constant_coeff;
+    };
+};
+
 #endif
--- main_hist.cpp
+++ main_hist.cpp
@@ -11,7 +11,7 @@ int main()
     srand(time(0));
 
     std::vector<int> indices;
-    for (int i=100; i<1001; i+=100)
+    for (int i=100; i<2001; i+=100)
     {
         indices.push_back(i);
     }
@@ -20,14 +20,14 @@ int main()
     fp = fopen("/home/btreece/Programs/BASIC_MPI/OUT_6_3_2019.txt","w");
 
     
-    brownian_particle b = brownian_particle(1001, 0.001, 1.0, 1.0, 1.0, 0.0, 0.0, 0.01, new Constant_Force(1.0));
+    brownian_particle b = brownian_particle(2001, 0.001, 1.0, 1.0, 1.0, 0.0, 0.0, 0.01, new Linear_Force(-1.618034, 2.718281));
 
     stat_list s_l = e_x | e_x2 | e_v | e_v2 | e_xv;
     brownian_histogram h = brownian_histogram(b, indices, s_l);
 //    stat_list s_l = e_x2 | e_v2;
 //    brownian_averages h = brownian_averages(b, s_l);
 
-    h.add_samples(1000000, 1.0);
+    h.add_samples(50000000, 1.0);
 //    h.add_samples(1000, 1.0);
     h.print_data_to_file(fp);
     fclose(fp);
